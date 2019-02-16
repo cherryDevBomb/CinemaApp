3a61b3da28d40fec86af880e4cdabea2dafe5933
viorelyo <viorelgurdis@gmail.com>
2019-02-16 03:15:05 +0200

    Added Film Banner to MakeReservationActivity

--- app/src/main/java/com/example/cinemaapp/MakeReservationActivity.java
+++ app/src/main/java/com/example/cinemaapp/MakeReservationActivity.java
@@ -3,14 +3,39 @@ package com.example.cinemaapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MakeReservationActivity extends AppCompatActivity {
+import com.example.cinemaapp.presenter.MakeReservationPresenter;
+
+public class MakeReservationActivity extends AppCompatActivity implements MakeReservationPresenter.MainView {
+    private MakeReservationPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_reservation);
 
+        presenter = new MakeReservationPresenter(this);
+
         getSupportActionBar().setTitle("Reserve");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public void showPlaces(int[][] places) {
+
+    }
+
+    @Override
+    public void setPoster(String moviePoster) {
+
+    }
+
+    @Override
+    public void setMovieTitle(String title) {
+
+    }
+
+    @Override
+    public void setStartTime(String time) {
+
     }
 }
