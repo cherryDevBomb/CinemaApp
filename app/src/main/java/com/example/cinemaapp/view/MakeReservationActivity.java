f1899ad06782bd68c6cd8e203624b72c1ff5d92a
cherryDevBomb <turcan.olguta@gmail.com>
2019-02-17 01:29:14 +0200

    film_card style updated

--- app/src/main/java/com/example/cinemaapp/view/MakeReservationActivity.java
+++ app/src/main/java/com/example/cinemaapp/view/MakeReservationActivity.java
@@ -1,9 +1,11 @@
 package com.example.cinemaapp.view;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.cinemaapp.R;
+import com.example.cinemaapp.model.Film;
 import com.example.cinemaapp.presenter.MakeReservationPresenter;
 
 public class MakeReservationActivity extends AppCompatActivity implements MakeReservationPresenter.MainView {
@@ -15,6 +17,13 @@ public class MakeReservationActivity extends AppCompatActivity implements MakeRe
         setContentView(R.layout.activity_make_reservation);
 
         presenter = new MakeReservationPresenter(this);
+
+        /////////////////////////////////////////////////////////
+        //retrieve values from Intent
+//        Intent creatorIntent = getIntent();
+//        Film filmObject = (Film)creatorIntent.getSerializableExtra("film");
+//        String time = creatorIntent.getStringExtra("time");
+        /////////////////////////////////////////////////////////
 
         getSupportActionBar().setTitle("Reserve");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
