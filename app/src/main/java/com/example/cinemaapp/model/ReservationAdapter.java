9c31d5674407d41a4e692cb6b25616bbf67c5bc7
viorelyo <viorelgurdis@gmail.com>
2019-02-20 03:15:54 +0200

    Bugs fixed

--- app/src/main/java/com/example/cinemaapp/model/ReservationAdapter.java
+++ app/src/main/java/com/example/cinemaapp/model/ReservationAdapter.java
@@ -1,8 +1,6 @@
 package com.example.cinemaapp.model;
 
-import android.annotation.SuppressLint;
 import android.app.Dialog;
-import android.content.Intent;
 import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.RequiresApi;
